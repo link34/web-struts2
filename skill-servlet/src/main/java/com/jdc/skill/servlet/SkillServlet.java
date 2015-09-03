bada6e1bdccc3679bc415ff4ce34147c2fa85d17
minlwin <lwin.zawmin@gmail.com>
2015-09-03 19:46:59 +0630

    share

---
+++ skill-servlet/src/main/java/com/jdc/skill/servlet/SkillServlet.java
@@ -0,0 +1,11 @@
+package com.jdc.skill.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class SkillServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+}
