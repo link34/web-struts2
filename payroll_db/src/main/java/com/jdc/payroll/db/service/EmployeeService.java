cfd1b7b9d2e2cb9bac814b44b9464174472334c0
minlwin <lwin.zawmin@gmail.com>
2015-09-08 20:07:40 +0630

    add EmployeeServiceImp Class

--- payroll_db/src/main/java/com/jdc/payroll/db/service/EmployeeService.java
+++ payroll_db/src/main/java/com/jdc/payroll/db/service/EmployeeService.java
@@ -1,9 +1,14 @@
 package com.jdc.payroll.db.service;
 
+import java.util.List;
+
 import com.jdc.payroll.db.entity.Employee;
 
 public interface EmployeeService {
 	
 	void create(Employee e);
+	Employee findById(String emp_cd);
+	List<Employee> findByName(String name);
+	List<Employee> getAllOrderByName();
 
 }
